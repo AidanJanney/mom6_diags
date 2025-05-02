e8263322823e6d66fca74f2d76d367eacd2d7296
Aidan Janney <aidan.janney@colorado.edu>
2025-05-02 12:46:20 -0600

    Package skeleton and some

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name="mom6_diags",
+        packages=["mom6_diags"],
+        version="0.1",
+        package_dir={"mom6_diags": "mom6_diags"},
+    )
